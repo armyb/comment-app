fc53e54dd2b7be55a5f0c61111dbca050f5bd626
周华健 <zhouhuajian@ly.com>
2017-04-13 16:07:32 +0800

    文字修改

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -12,7 +12,7 @@ class CommentApp extends Component{
     handleSubmitComment(comment){
         if(!comment)return;
         if(!comment.username) return alert('请输入用户名');
-        if(!comment.content) return alert(' 请 输入评论内容')
+        if(!comment.content) return alert('请输入评论内容')
         this.state.comments.push(comment);
         this.setState({
             comments:this.state.comments
